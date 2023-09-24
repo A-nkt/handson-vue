379d79f708106c371cb6b1ccd0b70c231f61b2b6
A-nkt <dsduoa31@gmail.com>
2023-09-24 13:42:59 +0900

    first commit

---
+++ components-basics/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import OneScreen from "./components/OneSection.vue"
+</script>
+
+<template>
+  <h1>コンポーネント基礎</h1>
+  <section>
+    <h2>コンポーネント１個</h2>
+    <OneScreen />
+  </section>
+  <section>
+    <h2>コンポーネントが複数</h2>
+    <OneScreen />
+    <OneScreen />
+    <OneScreen />
+  </section>
+</template>
+
+<style>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
---
+++ directive-bind-basic/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const msg = ref("まだです!")
+const onEntryKey = ():void => {
+  msg.value = "エンターキーが押されました"
+}
+const onRightButtonClick = ():void => {
+  msg.value = "ボタンが右クリックされました"
+}
+const onShiftClick = ():void => {
+  msg.value = "シフトを押しながらクリックされました"
+}
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <input type="text" v-on:keydown.enter="onEntryKey"><br>
+  <button v-on:click.right="onRightButtonClick">右クリック</button><br>
+  <button v-on:click.shift="onShigtClick">シフトを押しながらクリック</button><br>
+</template>
