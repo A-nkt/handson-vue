74c5ff822ba5ef19e2b6495697e3475e6bfc1b14
A-nkt <dsduoa31@gmail.com>
2023-10-01 15:47:36 +0900

    second commit

---
+++ pinia-basic/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { useCounterStore } from "@/stores/counter"
+
+const counterStore = useCounterStore()
+const count = computed(
+  () : number => {
+    return counterStore.counter;
+  }
+);
+
+const doubleCount = computed(
+  (): number => {
+    return counterStore.doubleCount
+  }
+)
+
+const onIncrementClick = () => {
+  counterStore.incrementCount()
+}
+</script>
+
+<template>
+    <p>現在のポイント : {{  count }}</p><br>
+    <p>現在のポイントをさらに倍：{{ doubleCount }}</p><br>
+    <button @click="onIncrementClick">加算</button>
+</template>
+
+<style scoped>
+</style>
---
+++ pinia-storage/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <h1>Pinia サンプル</h1>
+  </header>
+  <main>
+    <RouterView/>
+  </main>
+</template>
+
+<style>
+header{
+  position: fixed;
+  margin-top: 1rem;
+  width: 100%;
+  height: 50px;
+  top: 0;
+  z-index: 999;
+}
+main{
+  border: blue 1px solid;
+}
+
+#breadcrumbs ul li {
+  display: inline;
+  list-style-type: none;
+}
+
+#breadcrumbs {
+  margin-left: 0px;
+}
+
+#breadcrumbs ul{
+  padding-left: 0px;
+}
+
+#breadcrumbs ul .current{
+  color: red
+}
+
+#breadcrumbs ul li:before{
+  content: " > "
+}
+
+#breadcrumbs ul li:first-child:before{
+  content: none
+}
+</style>
